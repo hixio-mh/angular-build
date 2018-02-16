8decde60781520ec78304b8f7da19c9c889ab2ba
Moe Myat Zaw <mmzliveid@gmail.com>
2018-02-16 14:51:16 +0630

    chore: remove regexp-helper

--- src/utils/regexp-helpers.ts
+++
@@ -1,8 +0,0 @@
-export function getStringOrRegExp(str: string): RegExp | string {
-    if (!str || !str.length || str.length < 3 || !str.startsWith('/') || !str.endsWith('/')) {
-        return str;
-    }
-
-    const pattern = str.substr(1, str.length - 2);
-    return new RegExp(pattern, 'gi');
-}
