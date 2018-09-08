16c91cf678ebcadf6b1b123dcab5fc7b044c6d91
Moe Myat Zaw <mmzliveid@gmail.com>
2018-09-08 18:39:58 +0630

    Clean code

--- src/webpack-configs/app/dll.ts
+++ src/webpack-configs/app/dll.ts
@@ -15,182 +15,182 @@ import { getAppStylesWebpackConfigPartial } from './styles';
 const webpackMerge = require('webpack-merge');
 
 export function
-    getAppDllWebpackConfig<TConfig extends AppProjectConfigInternal>(angularBuildContext: AngularBuildContext<TConfig>):
-    webpack.Configuration {
-    const appConfig = angularBuildContext.projectConfig as AppProjectConfigInternal;
-    let customWebpackConfig: webpack.Configuration = {};
+  getAppDllWebpackConfig<TConfig extends AppProjectConfigInternal>(angularBuildContext: AngularBuildContext<TConfig>):
+  webpack.Configuration {
+  const appConfig = angularBuildContext.projectConfig as AppProjectConfigInternal;
+  let customWebpackConfig: webpack.Configuration = {};
 
-    if (!appConfig._isDll || angularBuildContext.projectConfig._projectType !== 'app') {
-        return {};
-    }
+  if (!appConfig._isDll || angularBuildContext.projectConfig._projectType !== 'app') {
+    return {};
+  }
 
-    if (appConfig.webpackConfig) {
-        const customWebpackConfigPath =
-            path.resolve(AngularBuildContext.workspaceRoot, appConfig.root || '', appConfig.webpackConfig);
-        customWebpackConfig =
-            getCustomWebpackConfig(customWebpackConfigPath, angularBuildContext) || {};
-    }
+  if (appConfig.webpackConfig) {
+    const customWebpackConfigPath =
+      path.resolve(AngularBuildContext.workspaceRoot, appConfig.root || '', appConfig.webpackConfig);
+    customWebpackConfig =
+      getCustomWebpackConfig(customWebpackConfigPath, angularBuildContext) || {};
+  }
 
-    const configs: webpack.Configuration[] = [
-        getAppCommonWebpackConfigPartial(angularBuildContext),
-        getAppStylesWebpackConfigPartial(angularBuildContext),
-        getAppDllWebpackConfigPartial(angularBuildContext),
-        customWebpackConfig
-    ];
+  const configs: webpack.Configuration[] = [
+    getAppCommonWebpackConfigPartial(angularBuildContext),
+    getAppStylesWebpackConfigPartial(angularBuildContext),
+    getAppDllWebpackConfigPartial(angularBuildContext),
+    customWebpackConfig
+  ];
 
-    const mergedConfig = webpackMerge(configs) as webpack.Configuration;
-    if (!mergedConfig.entry || (typeof mergedConfig.entry === 'object' && !Object.keys(mergedConfig.entry).length)) {
-        mergedConfig.entry = (() => ({})) as any;
-    }
+  const mergedConfig = webpackMerge(configs) as webpack.Configuration;
+  if (!mergedConfig.entry || (typeof mergedConfig.entry === 'object' && !Object.keys(mergedConfig.entry).length)) {
+    mergedConfig.entry = (() => ({})) as any;
+  }
 
-    return mergedConfig;
+  return mergedConfig;
 }
 
 function getAppDllWebpackConfigPartial<TConfig extends AppProjectConfigInternal>(angularBuildContext:
-    AngularBuildContext<TConfig>): webpack.Configuration {
-    const logLevel = angularBuildContext.buildOptions.logLevel;
+  AngularBuildContext<TConfig>): webpack.Configuration {
+  const logLevel = angularBuildContext.buildOptions.logLevel;
 
-    const appConfig = angularBuildContext.projectConfig as AppProjectConfigInternal;
+  const appConfig = angularBuildContext.projectConfig as AppProjectConfigInternal;
 
-    if (!appConfig.outputPath) {
-        throw new InvalidConfigError(
-            `The 'projects[${angularBuildContext.projectConfig.name || angularBuildContext.projectConfig._index
-            }].outputPath' value is required.`);
+  if (!appConfig.outputPath) {
+    throw new InvalidConfigError(
+      `The 'projects[${angularBuildContext.projectConfig.name || angularBuildContext.projectConfig._index
+      }].outputPath' value is required.`);
+  }
+
+  const projectRoot = path.resolve(AngularBuildContext.workspaceRoot, appConfig.root || '');
+  const outputPath = path.resolve(AngularBuildContext.workspaceRoot, appConfig.outputPath);
+
+  // TODO: to review for hash
+  const libHashFormat = (!appConfig.platformTarget || appConfig.platformTarget === 'web') &&
+    appConfig._outputHashing &&
+    appConfig._outputHashing.bundles
+    ? outputHashFormat.bundle
+    : '';
+
+  const libraryName = `[name]_${libHashFormat || 'lib'}`;
+  const vendorChunkName = appConfig.vendorChunkName || 'vendor';
+
+  const entryPoints: { [key: string]: string[] } = {};
+  const tsEntries: any[] = [];
+  const entries: string[] = [];
+
+  // dll
+  if (appConfig.vendors) {
+    if (!appConfig._dllParsedResult) {
+      throw new InternalError("The 'appConfig._dllParsedResult' is not set");
     }
+    const dllResult = appConfig._dllParsedResult;
+    dllResult.tsEntries.forEach(tsEntry => {
+      if (!tsEntries.includes(tsEntry)) {
+        tsEntries.push(tsEntry);
+      }
+      if (!entries.includes(tsEntry)) {
+        entries.push(tsEntry);
+      }
+    });
+    dllResult.scriptEntries.forEach(scriptEntry => {
+      if (!entries.includes(scriptEntry)) {
+        entries.push(scriptEntry);
+      }
+    });
+    dllResult.styleEntries.forEach(styleEntry => {
+      if (!entries.includes(styleEntry)) {
+        entries.push(styleEntry);
+      }
+    });
+  }
 
-    const projectRoot = path.resolve(AngularBuildContext.workspaceRoot, appConfig.root || '');
-    const outputPath = path.resolve(AngularBuildContext.workspaceRoot, appConfig.outputPath);
+  if (!entries.length) {
+    throw new InvalidConfigError(
+      `No entry available in 'apps[${angularBuildContext.projectConfig._index
+      }].dll'.`);
+  }
 
-    // TODO: to review for hash
-    const libHashFormat = (!appConfig.platformTarget || appConfig.platformTarget === 'web') &&
-        appConfig._outputHashing &&
-        appConfig._outputHashing.bundles
-        ? outputHashFormat.bundle
-        : '';
+  entryPoints[vendorChunkName] = entries;
 
-    const libraryName = `[name]_${libHashFormat || 'lib'}`;
-    const vendorChunkName = appConfig.vendorChunkName || 'vendor';
+  const rules: webpack.RuleSetRule[] = [];
 
-    const entryPoints: { [key: string]: string[] } = {};
-    const tsEntries: any[] = [];
-    const entries: string[] = [];
+  // plugins
+  const plugins: webpack.Plugin[] = [
+    new webpack.DllPlugin({
+      context: projectRoot,
+      path: path.resolve(outputPath, `${vendorChunkName}-manifest.json`),
+      name: libraryName
+    }),
+    new WriteStatsJsonWebpackPlugin({
+      outputPath: outputPath,
+      path: path.resolve(outputPath, `${vendorChunkName}-assets.json`),
+      forceWriteToDisk: true,
+      loggerOptions: {
+        logLevel: logLevel
+      }
+    }),
+    new WriteAssetsToDiskWebpackPlugin({
+      outputPath: outputPath,
+      emittedPaths: [path.resolve(outputPath, `${vendorChunkName}-manifest.json`)],
+      exclude: [`${vendorChunkName}-assets.json`],
+      loggerOptions: {
+        logLevel: logLevel
+      }
+    })
+  ];
 
-    // dll
-    if (appConfig.vendors) {
-        if (!appConfig._dllParsedResult) {
-            throw new InternalError("The 'appConfig._dllParsedResult' is not set");
-        }
-        const dllResult = appConfig._dllParsedResult;
-        dllResult.tsEntries.forEach(tsEntry => {
-            if (!tsEntries.includes(tsEntry)) {
-                tsEntries.push(tsEntry);
-            }
-            if (!entries.includes(tsEntry)) {
-                entries.push(tsEntry);
-            }
-        });
-        dllResult.scriptEntries.forEach(scriptEntry => {
-            if (!entries.includes(scriptEntry)) {
-                entries.push(scriptEntry);
-            }
-        });
-        dllResult.styleEntries.forEach(styleEntry => {
-            if (!entries.includes(styleEntry)) {
-                entries.push(styleEntry);
-            }
-        });
-    }
-
-    if (!entries.length) {
-        throw new InvalidConfigError(
-            `No entry available in 'apps[${angularBuildContext.projectConfig._index
-            }].dll'.`);
-    }
-
-    entryPoints[vendorChunkName] = entries;
-
-    const rules: webpack.RuleSetRule[] = [];
-
-    // plugins
-    const plugins: webpack.Plugin[] = [
-        new webpack.DllPlugin({
-            context: projectRoot,
-            path: path.resolve(outputPath, `${vendorChunkName}-manifest.json`),
-            name: libraryName
-        }),
-        new WriteStatsJsonWebpackPlugin({
-            outputPath: outputPath,
-            path: path.resolve(outputPath, `${vendorChunkName}-assets.json`),
-            forceWriteToDisk: true,
-            loggerOptions: {
-                logLevel: logLevel
-            }
-        }),
-        new WriteAssetsToDiskWebpackPlugin({
-            outputPath: outputPath,
-            emittedPaths: [path.resolve(outputPath, `${vendorChunkName}-manifest.json`)],
-            exclude: [`${vendorChunkName}-assets.json`],
-            loggerOptions: {
-                logLevel: logLevel
-            }
-        })
-    ];
-
-    if (tsEntries.length > 0) {
-        const tsConfigPath = appConfig._tsConfigPath;
-        const tsLoaderOptions: { [key: string]: any } = {
-            instance: `at-${appConfig.name || 'apps[' + appConfig._index + ']'}-loader`,
-            transpileOnly: appConfig.tsConfig ? false : true,
-            onlyCompileBundledFiles: appConfig.tsConfig ? false : true,
-            silent: logLevel !== 'debug'
-        };
-
-        if (appConfig.tsConfig && tsConfigPath) {
-            tsLoaderOptions.configFile = tsConfigPath;
-        }
-        if (logLevel) {
-            tsLoaderOptions.logLevel = logLevel;
-        }
-
-        const tsLoader = resolveLoaderPath('ts-loader');
-
-        rules.push({
-            test: /\.ts$/,
-            use: [
-                {
-                    loader: tsLoader,
-                    options: tsLoaderOptions
-                }
-            ],
-            include: tsEntries
-        });
-    }
-
-    // es6-promise
-    // workaround for https://github.com/stefanpenner/es6-promise/issues/100
-    plugins.push(new webpack.IgnorePlugin(/^vertx$/));
-
-    // workaround for https://github.com/andris9/encoding/issues/16
-    // commonPlugins.push(new webpack.NormalModuleReplacementPlugin(/\/iconv-loader$/, require.resolve('node-noop'))));
-
-    // angular fix plugins
-    plugins.push(...getAngularFixPlugins(angularBuildContext));
-
-    // webpack config
-    const webpackDllConfig: webpack.Configuration = {
-        entry: entryPoints,
-        resolve: {
-            extensions: tsEntries.length > 0 ? ['.ts', '.mjs', '.js'] : ['.mjs', '.js'],
-        },
-        output: {
-            publicPath: appConfig.publicPath || '/',
-            library: libraryName
-        },
-        module: {
-            rules: rules
-        },
-        plugins: plugins
+  if (tsEntries.length > 0) {
+    const tsConfigPath = appConfig._tsConfigPath;
+    const tsLoaderOptions: { [key: string]: any } = {
+      instance: `at-${appConfig.name || 'apps[' + appConfig._index + ']'}-loader`,
+      transpileOnly: appConfig.tsConfig ? false : true,
+      onlyCompileBundledFiles: appConfig.tsConfig ? false : true,
+      silent: logLevel !== 'debug'
     };
 
-    return webpackDllConfig;
+    if (appConfig.tsConfig && tsConfigPath) {
+      tsLoaderOptions.configFile = tsConfigPath;
+    }
+    if (logLevel) {
+      tsLoaderOptions.logLevel = logLevel;
+    }
+
+    const tsLoader = resolveLoaderPath('ts-loader');
+
+    rules.push({
+      test: /\.ts$/,
+      use: [
+        {
+          loader: tsLoader,
+          options: tsLoaderOptions
+        }
+      ],
+      include: tsEntries
+    });
+  }
+
+  // es6-promise
+  // workaround for https://github.com/stefanpenner/es6-promise/issues/100
+  plugins.push(new webpack.IgnorePlugin(/^vertx$/));
+
+  // workaround for https://github.com/andris9/encoding/issues/16
+  // commonPlugins.push(new webpack.NormalModuleReplacementPlugin(/\/iconv-loader$/, require.resolve('node-noop'))));
+
+  // angular fix plugins
+  plugins.push(...getAngularFixPlugins(angularBuildContext));
+
+  // webpack config
+  const webpackDllConfig: webpack.Configuration = {
+    entry: entryPoints,
+    resolve: {
+      extensions: tsEntries.length > 0 ? ['.ts', '.mjs', '.js'] : ['.mjs', '.js'],
+    },
+    output: {
+      publicPath: appConfig.publicPath || '/',
+      library: libraryName
+    },
+    module: {
+      rules: rules
+    },
+    plugins: plugins
+  };
+
+  return webpackDllConfig;
 }
--- src/webpack-configs/app/reference-dll.ts
+++ src/webpack-configs/app/reference-dll.ts
@@ -10,89 +10,89 @@ import { InvalidConfigError } from '../../error-models';
 import { getAppDllWebpackConfig } from './dll';
 
 export function
-    getAppReferenceDllWebpackConfigPartial<TConfig extends AppProjectConfigInternal>(angularBuildContext:
-        AngularBuildContext<TConfig>): webpack.Configuration {
-    const logLevel = angularBuildContext.buildOptions.logLevel;
+  getAppReferenceDllWebpackConfigPartial<TConfig extends AppProjectConfigInternal>(angularBuildContext:
+    AngularBuildContext<TConfig>): webpack.Configuration {
+  const logLevel = angularBuildContext.buildOptions.logLevel;
 
-    const appConfig = angularBuildContext.projectConfig as AppProjectConfigInternal;
+  const appConfig = angularBuildContext.projectConfig as AppProjectConfigInternal;
 
-    if (!appConfig.referenceDll || !appConfig.entry) {
-        return {};
+  if (!appConfig.referenceDll || !appConfig.entry) {
+    return {};
+  }
+
+  if (!appConfig.outputPath) {
+    throw new InvalidConfigError(
+      `The 'projects[${appConfig.name || appConfig._index}].outputPath' value is required.`);
+  }
+
+  const projectRoot = path.resolve(AngularBuildContext.workspaceRoot, appConfig.root || '');
+  const outputPath = path.resolve(AngularBuildContext.workspaceRoot, appConfig.outputPath);
+
+  const dllEnvironment = { ...angularBuildContext.buildOptions.environment };
+  dllEnvironment.dll = true;
+
+  if (dllEnvironment.aot) {
+    dllEnvironment.aot = false;
+  }
+
+  const dllProjectConfig =
+    JSON.parse(JSON.stringify(angularBuildContext.projectConfigWithoutEnvApplied)) as AppProjectConfigInternal;
+  applyProjectConfigWithEnvironment(dllProjectConfig, dllEnvironment);
+  dllProjectConfig._isDll = true;
+
+  const dllBuildOptions = { ...angularBuildContext.buildOptions };
+  dllBuildOptions.environment = dllEnvironment;
+
+  if (!dllProjectConfig.vendors || !dllProjectConfig.vendors.length) {
+    return {};
+  }
+
+  const plugins: webpack.Plugin[] = [];
+
+  const vendorChunkName = appConfig.vendorChunkName || 'vendor';
+  const manifests: { file: string; chunkName: string; }[] = [];
+  manifests.push(
+    {
+      file: path.resolve(outputPath, `${vendorChunkName}-manifest.json`),
+      chunkName: vendorChunkName
     }
+  );
 
-    if (!appConfig.outputPath) {
-        throw new InvalidConfigError(
-            `The 'projects[${appConfig.name || appConfig._index}].outputPath' value is required.`);
+  const dllAngularBuildContext = new AngularBuildContext({
+    workspaceRoot: AngularBuildContext.workspaceRoot,
+    host: angularBuildContext.host,
+    projectConfigWithoutEnvApplied: angularBuildContext.projectConfigWithoutEnvApplied,
+    projectConfig: dllProjectConfig,
+    buildOptions: dllBuildOptions
+  });
+
+  // dynamic dll
+  plugins.push(new DynamicDllWebpackPlugin({
+    manifests: manifests,
+    getDllConfigFunc: () => getAppDllWebpackConfig(dllAngularBuildContext),
+    loggerOptions: {
+      logLevel: logLevel
     }
+  }));
 
-    const projectRoot = path.resolve(AngularBuildContext.workspaceRoot, appConfig.root || '');
-    const outputPath = path.resolve(AngularBuildContext.workspaceRoot, appConfig.outputPath);
-
-    const dllEnvironment = { ...angularBuildContext.buildOptions.environment };
-    dllEnvironment.dll = true;
-
-    if (dllEnvironment.aot) {
-        dllEnvironment.aot = false;
-    }
-
-    const dllProjectConfig =
-        JSON.parse(JSON.stringify(angularBuildContext.projectConfigWithoutEnvApplied)) as AppProjectConfigInternal;
-    applyProjectConfigWithEnvironment(dllProjectConfig, dllEnvironment);
-    dllProjectConfig._isDll = true;
-
-    const dllBuildOptions = { ...angularBuildContext.buildOptions };
-    dllBuildOptions.environment = dllEnvironment;
-
-    if (!dllProjectConfig.vendors || !dllProjectConfig.vendors.length) {
-        return {};
-    }
-
-    const plugins: webpack.Plugin[] = [];
-
-    const vendorChunkName = appConfig.vendorChunkName || 'vendor';
-    const manifests: { file: string; chunkName: string; }[] = [];
-    manifests.push(
-        {
-            file: path.resolve(outputPath, `${vendorChunkName}-manifest.json`),
-            chunkName: vendorChunkName
-        }
+  let sourceType = dllProjectConfig.libraryTarget;
+  if (!sourceType && appConfig.platformTarget === 'node') {
+    sourceType = 'commonjs2';
+  }
+  manifests.forEach(manifest => {
+    plugins.push(
+      new webpack.DllReferencePlugin({
+        context: projectRoot,
+        manifest: manifest.file,
+        name: appConfig.platformTarget === 'node' ? `./${manifest.chunkName}` : undefined,
+        sourceType: sourceType
+      })
     );
+  });
 
-    const dllAngularBuildContext = new AngularBuildContext({
-        workspaceRoot: AngularBuildContext.workspaceRoot,
-        host: angularBuildContext.host,
-        projectConfigWithoutEnvApplied: angularBuildContext.projectConfigWithoutEnvApplied,
-        projectConfig: dllProjectConfig,
-        buildOptions: dllBuildOptions
-    });
+  const webpackReferenceDllConfig: webpack.Configuration = {
+    plugins: plugins
+  };
 
-    // dynamic dll
-    plugins.push(new DynamicDllWebpackPlugin({
-        manifests: manifests,
-        getDllConfigFunc: () => getAppDllWebpackConfig(dllAngularBuildContext),
-        loggerOptions: {
-            logLevel: logLevel
-        }
-    }));
-
-    let sourceType = dllProjectConfig.libraryTarget;
-    if (!sourceType && appConfig.platformTarget === 'node') {
-        sourceType = 'commonjs2';
-    }
-    manifests.forEach(manifest => {
-        plugins.push(
-            new webpack.DllReferencePlugin({
-                context: projectRoot,
-                manifest: manifest.file,
-                name: appConfig.platformTarget === 'node' ? `./${manifest.chunkName}` : undefined,
-                sourceType: sourceType
-            })
-        );
-    });
-
-    const webpackReferenceDllConfig: webpack.Configuration = {
-        plugins: plugins
-    };
-
-    return webpackReferenceDllConfig;
+  return webpackReferenceDllConfig;
 }
