ef7f523945077c3b0c1ce81311d1af0a63037ce6
Moe Myat Zaw <mmzliveid@gmail.com>
2019-03-20 09:57:30 +0630

    Refector code

--- src/helpers/get-custom-webpack-config.ts
+++ src/helpers/get-custom-webpack-config.ts
@@ -6,15 +6,26 @@ import { Configuration } from 'webpack';
 import { AngularBuildContext } from '../build-context';
 import { AppProjectConfigInternal } from '../models/internals';
 
-export function getCustomWebpackConfig(modulePath: string, angularBuildContext: AngularBuildContext<AppProjectConfigInternal>):
-    Configuration | null {
+export async function getCustomWebpackConfig(modulePath: string, angularBuildContext: AngularBuildContext<AppProjectConfigInternal>):
+    Promise<Configuration | null> {
     const customWebpackConfigModule = require(modulePath);
     if (customWebpackConfigModule && customWebpackConfigModule.default && typeof customWebpackConfigModule.default === 'function') {
-        return customWebpackConfigModule.default(angularBuildContext) as Configuration;
-    }
-    if (customWebpackConfigModule && typeof customWebpackConfigModule === 'function') {
-        return customWebpackConfigModule(angularBuildContext) as Configuration;
+        const wpConfig = customWebpackConfigModule.default(angularBuildContext);
+        if (wpConfig && wpConfig instanceof Promise) {
+            return wpConfig as Promise<Configuration>;
+        } else {
+            return Promise.resolve(wpConfig);
+        }
     }
 
-    return null;
+    if (customWebpackConfigModule && typeof customWebpackConfigModule === 'function') {
+        const wpConfig = customWebpackConfigModule(angularBuildContext);
+        if (wpConfig && wpConfig instanceof Promise) {
+            return wpConfig as Promise<Configuration>;
+        } else {
+            return Promise.resolve(wpConfig);
+        }
+    }
+
+    return Promise.resolve(null);
 }
