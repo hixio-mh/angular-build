50995f63977ab5754ceffe46dec0b3df357058c7
Moe Myat Zaw <mmzliveid@gmail.com>
2019-03-20 09:56:52 +0630

    Refector code

---
+++ src/architect/models/index.ts
@@ -0,0 +1,5 @@
+export * from './app-builder-options';
+export * from './app-config-compat';
+export * from './build-options-compat';
+export * from './lib-builder-options';
+export * from './project-config-compat';
