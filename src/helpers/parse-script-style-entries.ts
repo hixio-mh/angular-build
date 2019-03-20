ec4f1ed5ed49359e18d6f3c60c29eb0b42cb45dc
Moe Myat Zaw <mmzliveid@gmail.com>
2019-03-20 09:58:07 +0630

    Refector code

--- src/helpers/parse-script-style-entries.ts
+++ src/helpers/parse-script-style-entries.ts
@@ -1,17 +1,18 @@
 // tslint:disable:no-any
 // tslint:disable:no-unsafe-any
 
-import { existsSync } from 'fs';
 import * as path from 'path';
+
+import { pathExists } from 'fs-extra';
 
 import { GlobalEntry } from '../models';
 import { GlobalScriptStyleParsedEntry } from '../models/internals';
 
-export function parseScriptStyleEntries(extraEntries: string | (string | GlobalEntry)[],
+export async function parseScriptStyleEntries(extraEntries: string | (string | GlobalEntry)[],
     defaultEntry: string,
     workspaceRoot: string,
     nodeModulesPath: string | null | undefined,
-    projectRoot: string): GlobalScriptStyleParsedEntry[] {
+    projectRoot: string): Promise<GlobalScriptStyleParsedEntry[]> {
     if (!extraEntries || !extraEntries.length) {
         return [];
     }
@@ -21,59 +22,65 @@ export function parseScriptStyleEntries(extraEntries: string | (string | GlobalE
         typeof entry === 'object' ? { ...entry } : entry
     );
 
-    return clonedEntries
+    const mappedEntries = clonedEntries
         .map((extraEntry: string | GlobalEntry) =>
-            typeof extraEntry === 'object' ? extraEntry : { input: extraEntry })
-        .map((extraEntry: GlobalEntry) => {
-            const parsedEntry: GlobalScriptStyleParsedEntry = {
-                paths: [],
-                entry: '',
-                lazy: extraEntry.lazy
-            };
+            typeof extraEntry === 'object' ? extraEntry : { input: extraEntry });
 
-            const inputs = Array.isArray(extraEntry.input) ? extraEntry.input : [extraEntry.input];
-            parsedEntry.paths = [];
-            inputs.forEach(input => {
-                let resolvedPath = path.resolve(projectRoot, input);
+    const parsedEntries: GlobalScriptStyleParsedEntry[] = [];
 
-                if (nodeModulesPath &&
-                    !existsSync(resolvedPath) &&
-                    input.startsWith('~node_modules') &&
-                    existsSync(path.resolve(workspaceRoot, input.substr(1)))) {
-                    resolvedPath = path.resolve(workspaceRoot, input.substr(1));
-                } else if (nodeModulesPath &&
-                    !existsSync(resolvedPath) &&
-                    input.startsWith('~') &&
-                    existsSync(path.resolve(nodeModulesPath, input.substr(1)))) {
-                    resolvedPath = path.resolve(nodeModulesPath, input.substr(1));
-                } else if (!existsSync(resolvedPath) &&
-                    input.startsWith('~') &&
-                    existsSync(path.resolve(workspaceRoot, input.substr(1)))) {
-                    resolvedPath = path.resolve(workspaceRoot, input.substr(1));
-                }
 
-                parsedEntry.paths.push(resolvedPath);
-            });
+    for (const extraEntry of mappedEntries) {
+        const parsedEntry: GlobalScriptStyleParsedEntry = {
+            paths: [],
+            entry: '',
+            lazy: extraEntry.lazy
+        };
 
-            if (extraEntry.bundleName) {
-                if (/(\\|\/)$/.test(extraEntry.bundleName) &&
-                    !Array.isArray(extraEntry.input) &&
-                    typeof extraEntry.input === 'string') {
-                    parsedEntry.entry = extraEntry.bundleName +
-                        path.basename(extraEntry.input)
-                            .replace(/\.(ts|js|less|sass|scss|styl|css)$/i, '');
-                } else {
-                    parsedEntry.entry = extraEntry.bundleName.replace(/\.(js|css)$/i, '');
-                }
-            } else if (extraEntry.lazy &&
-                !Array.isArray(extraEntry.input) &&
-                typeof extraEntry.input === 'string') {
-                parsedEntry.entry = path.basename(extraEntry.input)
-                    .replace(/\.(js|ts|css|scss|sass|less|styl)$/i, '');
-            } else {
-                parsedEntry.entry = defaultEntry;
+        const inputs = Array.isArray(extraEntry.input) ? extraEntry.input : [extraEntry.input];
+        parsedEntry.paths = [];
+        for (const input of inputs) {
+            let resolvedPath = path.resolve(projectRoot, input);
+
+            if (nodeModulesPath &&
+                !await pathExists(resolvedPath) &&
+                input.startsWith('~node_modules') &&
+                await pathExists(path.resolve(workspaceRoot, input.substr(1)))) {
+                resolvedPath = path.resolve(workspaceRoot, input.substr(1));
+            } else if (nodeModulesPath &&
+                !await pathExists(resolvedPath) &&
+                input.startsWith('~') &&
+                await pathExists(path.resolve(nodeModulesPath, input.substr(1)))) {
+                resolvedPath = path.resolve(nodeModulesPath, input.substr(1));
+            } else if (!await pathExists(resolvedPath) &&
+                input.startsWith('~') &&
+                await pathExists(path.resolve(workspaceRoot, input.substr(1)))) {
+                resolvedPath = path.resolve(workspaceRoot, input.substr(1));
             }
 
-            return parsedEntry;
-        });
+            parsedEntry.paths.push(resolvedPath);
+        }
+
+        if (extraEntry.bundleName) {
+            if (/(\\|\/)$/.test(extraEntry.bundleName) &&
+                !Array.isArray(extraEntry.input) &&
+                typeof extraEntry.input === 'string') {
+                parsedEntry.entry = extraEntry.bundleName +
+                    path.basename(extraEntry.input)
+                        .replace(/\.(ts|js|less|sass|scss|styl|css)$/i, '');
+            } else {
+                parsedEntry.entry = extraEntry.bundleName.replace(/\.(js|css)$/i, '');
+            }
+        } else if (extraEntry.lazy &&
+            !Array.isArray(extraEntry.input) &&
+            typeof extraEntry.input === 'string') {
+            parsedEntry.entry = path.basename(extraEntry.input)
+                .replace(/\.(js|ts|css|scss|sass|less|styl)$/i, '');
+        } else {
+            parsedEntry.entry = defaultEntry;
+        }
+
+        parsedEntries.push(parsedEntry);
+    }
+
+    return parsedEntries;
 }
