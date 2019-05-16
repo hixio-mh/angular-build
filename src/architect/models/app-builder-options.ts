7feb3e6785e31b90473e82b8337a80cf68f16f60
Moe Myat Zaw <mmzliveid@gmail.com>
2019-05-16 17:23:09 +0630

    Update app-builder-options.ts

--- src/architect/models/app-builder-options.ts
+++ src/architect/models/app-builder-options.ts
@@ -1,6 +1,5 @@
 import { AppProjectConfig, BuildOptions } from '../../models';
 
 import { AppConfigCompat } from './app-config-compat';
-import { BuildOptionsCompat } from './build-options-compat';
 
-export interface AppBuilderOptions extends AppProjectConfig, AppConfigCompat, BuildOptions, BuildOptionsCompat { }
+export interface AppBuilderOptions extends AppProjectConfig, BuildOptions, AppConfigCompat { }
