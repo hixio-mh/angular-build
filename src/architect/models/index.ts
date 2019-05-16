f366094640b14745d009f50bcf767013151ea900
Moe Myat Zaw <mmzliveid@gmail.com>
2019-05-16 17:24:48 +0630

    Update compat model

--- src/architect/models/index.ts
+++ src/architect/models/index.ts
@@ -1,5 +1,3 @@
 export * from './app-builder-options';
 export * from './app-config-compat';
-export * from './build-options-compat';
 export * from './lib-builder-options';
-export * from './project-config-compat';
