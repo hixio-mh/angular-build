b3ffe02bcae921bb91511cb627787ff46f231629
Moe Myat Zaw <mmzliveid@gmail.com>
2020-05-28 00:59:49 +0630

    Update package-entrypoints.ts

--- src/models/internals/package-entrypoints.ts
+++ src/models/internals/package-entrypoints.ts
@@ -4,6 +4,7 @@ export interface PackageEntrypoints {
     module?: string;
     es2015?: string;
     esm5?: string;
+    // It is deprecated as of v9, might be removed in the future.
     esm2015?: string;
     fesm2015?: string;
     fesm5?: string;
